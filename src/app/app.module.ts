bf11a6d2ba5305275cac8b98a509ffd331e3a4dc
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-02-09 12:21:30 -0500

    created all high level components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { DescriptionComponent } from './description/description.component';
+import { BoardComponent } from './board/board.component';
+import { SwitchComponent } from './switch/switch.component';
+import { NewGameComponent } from './new-game/new-game.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    DescriptionComponent,
+    BoardComponent,
+    SwitchComponent,
+    NewGameComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
