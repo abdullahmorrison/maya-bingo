c58494a730de738996871cd012677d547c2fa4d9
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-02-26 10:34:36 -0500

    board-tile component created

---
+++ src/app/board-tile/board-tile.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoardTileComponent } from './board-tile.component';
+
+describe('BoardTileComponent', () => {
+  let component: BoardTileComponent;
+  let fixture: ComponentFixture<BoardTileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BoardTileComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BoardTileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/board-tile/board-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-tile',
+  templateUrl: './board-tile.component.html',
+  styleUrls: ['./board-tile.component.scss']
+})
+export class BoardTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
