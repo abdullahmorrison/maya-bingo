64dcf92962e25797022a6406e2e886b3f2f35df5
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-03-18 15:41:35 -0400

    board reducer update

--- src/store/board/board.reducers.ts
+++ src/store/board/board.reducers.ts
@@ -1,14 +1,17 @@
 import { Board } from './board.model';
 import { createReducer, on } from '@ngrx/store'
 import * as BoardActions from './board.actions'
+import alveusBoard from '../../assets/board/alveus.json'
+import desktopBoard from '../../assets/board/desktop.json'
 
 export const initialState: Board = {
-    tiles: [],
+    tiles: alveusBoard,
     bingo: false,
     goForBlackout: false
 }
 
 export const boardReducer = createReducer(
     initialState,
-    on(BoardActions.bingo, state => ({...state, bingo: true}))
+    on(BoardActions.bingo, state => ({...state, bingo: true})),
+    on(BoardActions.goForBlackout, state => ({...state, goForBlackout: true}))
 )
