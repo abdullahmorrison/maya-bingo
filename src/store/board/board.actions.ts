b9fcb8f0d7942e5359167dfd4e88d84afb890847
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-03-18 15:41:19 -0400

    updated board actions

--- src/store/board/board.actions.ts
+++ src/store/board/board.actions.ts
@@ -1,11 +1,6 @@
 import { createAction } from "@ngrx/store";
 
-export const addChip = createAction('[Board Tile] Add Chip')
-export const removeChip = createAction('[Board Tile] Remove Chip')
-
 export const bingo = createAction('[Board] Bingo')
-
-export const newGame = createAction('[New Game Button] New Game')
-
-export const goForBlackout = createAction('[Modal] Go For Blackout')
-export const switchStream = createAction('[Switch] Switch Stream')
+export const newGame = createAction('[Board] New Game')
+export const goForBlackout = createAction('[Board] Go For Blackout')
+export const switchStream = createAction('[Board] Switch Stream')
