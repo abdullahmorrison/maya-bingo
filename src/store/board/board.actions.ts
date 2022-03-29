35be750f65398110a1cbde2d550f6cdad35ffb87
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-03-28 21:05:43 -0400

    updated switch board action

--- src/store/board/board.actions.ts
+++ src/store/board/board.actions.ts
@@ -3,4 +3,4 @@ import { createAction } from "@ngrx/store";
 export const bingo = createAction('[Board] Bingo')
 export const newGame = createAction('[Board] New Game')
 export const goForBlackout = createAction('[Board] Go For Blackout')
-export const switchStream = createAction('[Board] Switch Stream')
+export const switchBoard = createAction('[Board] Switch Stream')
