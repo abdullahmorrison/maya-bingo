5d7b906db026295aad404b5482a4b7d37c15b9d0
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-03-29 19:23:22 -0400

    changing nav logo and alt text when switching boards

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,3 +1,9 @@
+import { Store } from '@ngrx/store';
+
+import { Board } from './../../store/board/board.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +12,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  boardType: Observable<"Alveus" | "Desktop">
+  logo: string 
+  altText: string 
 
-  constructor() { }
+  constructor(private store: Store<{board: Board}>) { 
+    //Switching the nav logo and alt text based on the board type
+    this.boardType = this.store.select('board').pipe(map(board => board.type))
+    this.logo = ""
+    this.altText = ""
 
-  ngOnInit(): void {
+    this.boardType.subscribe(type => {
+      if (type == "Alveus") {
+        this.logo = "../../assets/nav/alveus.svg"
+        this.altText = "mayahi3 Emote"
+      } else {
+        this.logo = "../../assets/nav/desktop.svg"
+        this.altText = "mayahiComfy Emote"
+      }
+    })
   }
 
+  ngOnInit(): void { }
 }
