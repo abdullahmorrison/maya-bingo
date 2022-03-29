96a19c146c8758de8911735d5bd6a2dd1afa9650
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-03-28 21:05:59 -0400

    added switch board reducer func

--- src/store/board/board.reducers.ts
+++ src/store/board/board.reducers.ts
@@ -1,11 +1,14 @@
-import { Board } from './board.model';
 import { createReducer, on } from '@ngrx/store'
+
+import { Board } from './board.model';
 import * as BoardActions from './board.actions'
+
 import alveusBoard from '../../assets/board/alveus.json'
 import desktopBoard from '../../assets/board/desktop.json'
 
 export const initialState: Board = {
-    tiles: alveusBoard,
+    type: 'Alveus',
+    tiles: alveusBoard.tiles,
     bingo: false,
     goForBlackout: false
 }
@@ -13,5 +16,11 @@ export const initialState: Board = {
 export const boardReducer = createReducer(
     initialState,
     on(BoardActions.bingo, state => ({...state, bingo: true})),
-    on(BoardActions.goForBlackout, state => ({...state, goForBlackout: true}))
+    on(BoardActions.goForBlackout, state => ({...state, goForBlackout: true})),
+    on(BoardActions.switchBoard, state => 
+        ({...state, 
+            type: state.type == "Alveus" ? "Desktop" : "Alveus", 
+            tiles: state.type == 'Alveus'? desktopBoard.tiles : alveusBoard.tiles
+        })
+    ),
 )
