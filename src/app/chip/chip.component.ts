6566e6e98cf6ce37fba58d95208e6b082ffff136
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-03-29 19:33:35 -0400

    chip image & alt text changing based on board type

--- src/app/chip/chip.component.ts
+++ src/app/chip/chip.component.ts
@@ -1,3 +1,9 @@
+import { Store } from '@ngrx/store';
+
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Board } from './../../store/board/board.model';
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +12,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chip.component.scss']
 })
 export class ChipComponent implements OnInit {
+  boardType: Observable<"Alveus" | "Desktop">
+  chipImage: string 
+  altText: string 
 
-  constructor() { }
+  constructor(private store: Store<{board: Board}>) { 
+    //Switching the chip logo and alt text based on the board type
+    this.boardType = this.store.select('board').pipe(map(board => board.type))
+    this.chipImage = ""
+    this.altText = ""
 
-  ngOnInit(): void {
+    this.boardType.subscribe(type => {
+      if (type == "Alveus") {
+        this.chipImage = "../../assets/chip/alveus.svg"
+        this.altText = "mayahiHappy Emote"
+      } else {
+        this.chipImage = "../../assets/chip/desktop.svg"
+        this.altText = "mayahiCozy Emote"
+      }
+    })
   }
-
+  ngOnInit(): void { }
 }
