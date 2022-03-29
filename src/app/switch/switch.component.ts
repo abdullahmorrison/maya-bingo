d62ea851cf2a8ea3ccebcc1e0891ba7bf797545d
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-03-29 13:49:59 -0400

    changing document colors through switch

--- src/app/switch/switch.component.ts
+++ src/app/switch/switch.component.ts
@@ -13,7 +13,20 @@ export class SwitchComponent implements OnInit {
 
   ngOnInit(): void { }
 
-  handleClick(){
+  handleClick(): void{
+    //change the board data
     this.store.dispatch(BoardActions.switchBoard()) 
+
+    //Change document colors
+    let primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--primary-color').trim()
+    if(primaryColor == '#636A60'){
+      document.documentElement.style.setProperty('--primary-color', '#FF9BD7');
+      document.documentElement.style.setProperty('--secondary-color', '#FFF6FE');
+      document.documentElement.style.setProperty('--tertiary-color', '#FFD1ED');
+    }else{
+      document.documentElement.style.setProperty('--primary-color', '#636A60');
+      document.documentElement.style.setProperty('--secondary-color', '#F9EEE7');
+      document.documentElement.style.setProperty('--tertiary-color', '#A8A8A8');
+    }
   }
 }
