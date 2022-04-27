e4c9f2afed850716f9ecb68c840f5af08657914a
Abdullah Morrison <abdullahmorrison@gmail.com>
2022-04-27 13:58:25 -0400

    added winning tiles to modal

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,9 +1,11 @@
+import { Tile } from './../../store/tile/tile.model';
 import { Board } from './../../store/board/board.model';
 import { Component, OnInit } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import * as BoardActions from '../../store/board/board.actions';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-modal',
@@ -11,13 +13,17 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent implements OnInit {
-  bingo$ = this.store.select('board').pipe(map(board => board.bingo))
+  bingo$: Observable<boolean>
+  winningTiles$: Observable<Tile[]>
 
-  constructor(private store: Store<{board: Board}>) { }
+  constructor(private store: Store<{ board: Board }>) {
+    this.bingo$ = this.store.select('board').pipe(map(board => board.bingo))
+    this.winningTiles$ = this.store.select('board').pipe(map(board => board.winningTiles))
+  }
 
   ngOnInit(): void { }
 
-  newGame(){
+  newGame() {
     this.store.dispatch(BoardActions.newGame())
   }
 }
